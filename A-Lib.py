c46850925c4b8544ceac20e96b1416ba341efe9b
Zepeacedust <arnor0903@gmail.com>
2021-04-01 00:50:00 +0000

    smá reformatting

--- A-Lib.py
+++ A-Lib.py
@@ -5,25 +5,27 @@ class TreeNode:
         self.left = None
         self.right = None
 
+
 class PrioQueue:
     def __init__(self, key=int):
         self.root = None
         self.key = key
-    
+
     def getEmpty(self):
         return self.root == None
     empty = property(getEmpty)
+
     def add(self, data):
         element = TreeNode(data)
-        #Classic add í Binary tree
+        # Classic add í Binary tree
         if self.root == None:
-            #Ef tré er tómt
+            # Ef tré er tómt
             self.root = element
             return 0
         else:
             head = self.root
             while True:
-                #Finna réttan stað og setja node þar
+                # Finna réttan stað og setja node þar
                 if self.key(element.data) <= self.key(head.data):
                     if head.left == None:
                         head.left = element
@@ -36,15 +38,16 @@ class PrioQueue:
                         return 0
                     else:
                         head = head.right
+
     def pop(self):
-        #TODO error Handling
-        
+        # TODO error Handling
+
         head = self.root
         last = None
         while head.left != None:
             last = head
             head = head.left
-        
+
         if last == None:
             self.root = head.right
         else:
@@ -52,6 +55,7 @@ class PrioQueue:
         data = head.data
         del head
         return data
+
 
 class Vector:
     def __init__(self, x, y):
@@ -66,8 +70,10 @@ class Vector:
 
     def __str__(self):
         return "(" + str(self.x) + ", " + str(self.y) + ")"
+
     def __eq__(self, other):
         return self.x == other.x and self.y == other.y
+
 
 def CountList(L):
     counted = {}
